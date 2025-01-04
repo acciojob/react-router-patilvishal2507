4e3207943fb7f5322dfe87a91de824fccc50cb5a
patilvishal2507 <117289866+patilvishal2507@users.noreply.github.com>
2025-01-04 23:27:24 +0530

    Update and rename About.jsx to About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,13 @@
 
 import React from "react";
 
-const About = () => {
+function About() {
   return (
     <div>
       <h1>About</h1>
       <p>This is a sample React Router program.</p>
     </div>
   );
-};
+}
 
 export default About;
